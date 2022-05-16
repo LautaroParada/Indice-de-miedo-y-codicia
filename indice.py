60d2675c300a817e64f0b3e7c12e819f6cb9cab6
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-05-16 17:13:46 -0400

    Update indice.py
    graficos iniciales

--- indice.py
+++ indice.py
@@ -124,26 +124,89 @@ dolar_ = np.log(dolar) - np.log(dolar.shift(20))
 ipsa_ = np.log(ipsa) - np.log(ipsa.shift(20))
 
 fng = pd.DataFrame()
-fng['safe_heaven'] = dolar_ / ipsa_
+fng['safe_heaven'] = (dolar_ - ipsa_) * 100
+
+# Graficar los datos
+fig, ax = plt.subplots(figsize=(10, 5))
+
+ax.plot(fng['safe_heaven'], color='tab:blue')
+ax.grid(True, linestyle='--')
+fig.suptitle('Dolar menos IPSA', fontweight='bold')
+plt.title('Retornos mensuales')
+ax.set_ylabel('')
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+
+# Graph source
+ax.text(0.2, -0.12,  
+         "Fuente: Banco Central de Chile   Gráfico: Lautaro Parada", 
+         horizontalalignment='center',
+         verticalalignment='center', 
+         transform=ax.transAxes, 
+         fontsize=8, 
+         color='black',
+         bbox=dict(facecolor='tab:gray', alpha=0.5))
+
+plt.show()
 
 del dolar, ipsa, dolar_, ipsa_
 
 #%% Junk Bond demand
 
-embi = cleaner('F019.SPS.PBP.91.D').fillna(method='ffill')
-fng['spreads_junk'] = np.log(embi) - np.log(embi.shift(1))
+fng['spreads_junk'] = cleaner('F019.SPS.PBP.91.D').fillna(method='ffill')
 
-del embi
+# Graficar los datos
+fig, ax = plt.subplots(figsize=(10, 5))
+
+ax.plot(fng['spreads_junk'], color='tab:blue')
+ax.grid(True, linestyle='--')
+fig.suptitle('Spread bonos soberanos', fontweight='bold')
+plt.title('')
+ax.set_ylabel('promedio, puntos base')
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+
+# Graph source
+ax.text(0.2, -0.12,  
+         "Fuente: Banco Central de Chile   Gráfico: Lautaro Parada", 
+         horizontalalignment='center',
+         verticalalignment='center', 
+         transform=ax.transAxes, 
+         fontsize=8, 
+         color='black',
+         bbox=dict(facecolor='tab:gray', alpha=0.5))
+
+plt.show()
+
 #%% Momentum del mercado 
 
 ipsa = cleaner('F013.IBC.IND.N.7.LAC.CL.CLP.BLO.D').fillna(method='ffill')
 
-ipsa_20 = np.log(ipsa) - np.log(ipsa.shift(20))
-ipsa_60 = np.log(ipsa) - np.log(ipsa.shift(60))
+ipsa_125 = ipsa.rolling(window=125, min_periods=125).mean()
 
-fng['momentum'] = ipsa_20 / ipsa_60
+fng['momentum'] = ipsa - ipsa_125
 
-del ipsa, ipsa_20, ipsa_60
+# Graficar los datos
+fig, ax = plt.subplots(figsize=(10, 5))
+
+ax.plot(fng['momentum'], color='tab:blue')
+ax.grid(True, linestyle='--')
+fig.suptitle('Momentum', fontweight='bold')
+plt.title('')
+ax.set_ylabel('')
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+
+# Graph source
+ax.text(0.2, -0.12,  
+         "Fuente: Banco Central de Chile   Gráfico: Lautaro Parada", 
+         horizontalalignment='center',
+         verticalalignment='center', 
+         transform=ax.transAxes, 
+         fontsize=8, 
+         color='black',
+         bbox=dict(facecolor='tab:gray', alpha=0.5))
+
+plt.show()
+
+del ipsa, ipsa_125
 
 #%% Volatilidad
 
@@ -153,7 +216,29 @@ ipsa = cleaner('F013.IBC.IND.N.7.LAC.CL.CLP.BLO.D').fillna(method='ffill')
 ipsa_vol_20 = ipsa.rolling(window=20, min_periods=20).std(ddof=0)
 ipsa_vol_60 = ipsa.rolling(window=60, min_periods=60).std(ddof=0)
 
-fng['volatilidad'] = ipsa_vol_20 / ipsa_vol_60
+fng['volatilidad'] = ipsa_vol_20 - ipsa_vol_60
+
+# Graficar los datos
+fig, ax = plt.subplots(figsize=(10, 5))
+
+ax.plot(fng['volatilidad'], color='tab:blue')
+ax.grid(True, linestyle='--')
+fig.suptitle('volatilidad', fontweight='bold')
+plt.title('')
+ax.set_ylabel('')
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+
+# Graph source
+ax.text(0.2, -0.12,  
+         "Fuente: Banco Central de Chile   Gráfico: Lautaro Parada", 
+         horizontalalignment='center',
+         verticalalignment='center', 
+         transform=ax.transAxes, 
+         fontsize=8, 
+         color='black',
+         bbox=dict(facecolor='tab:gray', alpha=0.5))
+
+plt.show()
 
 del ipsa, ipsa_vol_20, ipsa_vol_60
 
@@ -161,3 +246,43 @@ del ipsa, ipsa_vol_20, ipsa_vol_60
 
 fng['sentimiento'] = cleaner('F029.IDIE.IND.TOT.D').fillna(method='ffill')
 
+# Graficar los datos
+fig, ax = plt.subplots(figsize=(10, 5))
+
+ax.plot(fng['sentimiento'], color='tab:blue')
+ax.grid(True, linestyle='--')
+fig.suptitle('sentimiento', fontweight='bold')
+plt.title('')
+ax.set_ylabel('')
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+
+# Graph source
+ax.text(0.2, -0.12,  
+         "Fuente: Banco Central de Chile   Gráfico: Lautaro Parada", 
+         horizontalalignment='center',
+         verticalalignment='center', 
+         transform=ax.transAxes, 
+         fontsize=8, 
+         color='black',
+         bbox=dict(facecolor='tab:gray', alpha=0.5))
+
+plt.show()
+
+#%% Rescalar las variables
+
+fng.dropna(inplace=True)
+
+def rescale(values, new_min:int=0, new_max:int=100):
+    
+    old_max, old_min = max(values), min(values)
+    return [(new_max - new_min) / (old_max - old_min) * (x - old_min) + new_min for x in values]
+
+fng['res_safe_heaven'] = rescale(fng['safe_heaven'])
+fng['res_spreads_junk'] = rescale(fng['spreads_junk'])
+fng['res_momentum'] = rescale(fng['momentum'])
+fng['res_volatilidad'] = rescale(fng['volatilidad'])
+fng['res_sentimiento'] = rescale(fng['sentimiento'])
+
+fng['index'] = fng['res_safe_heaven']*0.13 + fng['res_spreads_junk']*0.13 + fng['res_momentum']*0.28 + fng['res_volatilidad']*0.28 + fng['res_sentimiento']*0.18
+
+plt.plot(fng['index'])
